ee0e44befc0f97d272f0887792d5d5ac085d1944
srmward <srmward@gmail.com>
2019-12-09 22:09:04 -0800

    Progress

--- generate_blog_template/src/template_builder.rs
+++ generate_blog_template/src/template_builder.rs
@@ -1,3 +1,20 @@
+//TODO config to specify file path
+
+use std::fs::File;
+use std::io::prelude::*;
+
 pub fn generate_template() {
   println!("Got it. Generating template now ...");
+  let result = create_file();
+}
+
+fn create_file() -> std::io::Result<()> {
+  let mut file = File::create("foo.txt")?;
+  file.write_all(
+    b"---
+  title: 'Title'
+  date: '2019-02-07'
+  ---",
+  )?;
+  Ok(())
 }
