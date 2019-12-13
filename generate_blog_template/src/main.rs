601075c3937799959a5af1b1c0cfefc1bc0817e4
srmward <srmward@gmail.com>
2019-12-12 21:56:06 -0800

    Include Title Grab

--- generate_blog_template/src/main.rs
+++ generate_blog_template/src/main.rs
@@ -3,9 +3,9 @@ mod template;
 
 fn main() {
     let article_name = String::from(reader::get_article_name());
-
+    let article_title = String::from(reader::get_article_title());
     // TODO: write confirm handler
-    reader::confirm(&article_name);
+    reader::confirm(&article_name, &article_title);
 
     template::generate(&article_name);
 }
--- generate_blog_template/src/reader.rs
+++ generate_blog_template/src/reader.rs
@@ -6,8 +6,15 @@ pub fn get_article_name() -> String {
   return format_name(handle_input());
 }
 
-pub fn confirm(article_name: &str) -> String {
-  println!("\nYou Typed: {}", article_name);
+pub fn get_article_title() -> String {
+  println!("\nName of the blog title: ");
+
+  return format_name(handle_input());
+}
+
+pub fn confirm(article_name: &str, article_title: &str) -> String {
+  println!("\nArticle Name You Typed: {}", article_name);
+  println!("\nArticle Title You Typed: {}", article_title);
   println!("\nConfirm: y/N:  ");
 
   return handle_input();
